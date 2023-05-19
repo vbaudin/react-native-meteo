90ddee84bf093d78a4ecf9dc8d27397e81f21950
vbaudin <vbaudin.dev@gmail.com>
2023-05-19 17:43:01 +0200

    initial push

---
+++ pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { s } from "./Home.style";
+
+const Home = () => {
+  return (
+    <>
+      <Text>Mon app météo</Text>
+    </>
+  );
+};
+
+export default Home;
