aace366795e7b354b8363cd246b069e10c3626f8
vbaudin <vbaudin.dev@gmail.com>
2023-05-20 12:47:56 +0200

    feat: meteo fetcher api

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,7 +1,50 @@
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+
 import { Text, View } from "react-native";
 import { s } from "./Home.style";
+import { useEffect, useState } from "react";
+
+import { fetchWeatherFromCoords } from "../../api/meteo";
 
 const Home = () => {
+  const [coords, setCoords] = useState();
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    getUserCoords();
+  }, []);
+
+  useEffect(() => {
+    if (coords) {
+      fetchWeather(coords);
+    }
+  }, [coords]);
+
+  // MeteoAPI.toto();
+
+  const getUserCoords = async () => {
+    const { status } = await requestForegroundPermissionsAsync();
+
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoords({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoords({ lat: "48.85", lng: "2.35" });
+    }
+    console.log("***", coords);
+  };
+
+  const fetchWeather = async (coordinates) => {
+    const weatherResponse = await fetchWeatherFromCoords(coordinates);
+    setWeather(weatherResponse);
+  };
+
   return (
     <>
       <View style={s.meteo_basic}>
